0438869a88a17cbe6fc33341b44c7410e8cc431f
PepeYerbes <ppyerbes@gmail.com>
2025-06-20 20:52:59 -0600

    Tarea completada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListardoctoresComponent } from "./componentes/listar-doctores/listar-doctores.component";
-import { AgrearDoctorComponent } from './componentes/agrear-doctor/agrear-doctor.component';
+import { MenuComponent } from './componentes/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ListardoctoresComponent, AgrearDoctorComponent],
+  imports: [MenuComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ListardoctoresComponent } from './componentes/listar-doctores/listar-doctores.component';
+import { AgrearDoctorComponent } from './componentes/agrear-doctor/agrear-doctor.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'listar', component: ListardoctoresComponent},
+    {path: 'agregar', component: AgrearDoctorComponent},
+    {path: '', redirectTo: 'listar', pathMatch: 'full'},
+    {path: '**', redirectTo: 'listar'}
+    
+];
